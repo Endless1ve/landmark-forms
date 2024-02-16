bf52bce6ac1ccfaa06d68436bfd8067f30cd7da8
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-02-16 14:02:39 +0300

    initialize webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Landmark Forms",
+      template: path.resolve(__dirname, "./src/template.html"), // шаблон
+      filename: "index.html", // название выходного файла
+    }),
+    new MiniCssExtractPlugin({ filename: "style.[contenthash].css" }),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
