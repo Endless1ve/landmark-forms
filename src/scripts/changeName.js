d3c7683e76a56d9d2e709cb0f8719cafee28ab48
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-03-06 13:42:32 +0300

    change file name change pattern

--- src/scripts/changeName.js
+++ src/scripts/changeName.js
@@ -3,8 +3,8 @@ export default function changeName(parentElem, inputElem) {
   let fileValue = inputElem.files[0].name;
   let name = fileValue.slice(0, -4);
   const resolution = fileValue.slice(-4);
-  if (name.length > 20) {
-    name = name.slice(0, 20) + "...";
+  if (name.length > 15) {
+    name = name.slice(0, 15) + "...";
     fileValue = name + resolution;
   }
   fileLabel.textContent = fileValue;
